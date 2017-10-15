c2ddfb245e120a58a3580755dae74f402bdd8961
Robin Glauser <robin.glauser@gmail.com>
2017-10-15 15:57:34 +0200

    Setting look for other operating systems.

--- src/main/java/ch/robinglauser/bfhexercise/tictactoe/TicTacToeClient.java
+++ src/main/java/ch/robinglauser/bfhexercise/tictactoe/TicTacToeClient.java
@@ -1,5 +1,6 @@
 package ch.robinglauser.bfhexercise.tictactoe;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,6 +12,18 @@ import java.util.Scanner;
 public class TicTacToeClient {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
 
         ClientGUI clientGUI = new ClientGUI();
 
