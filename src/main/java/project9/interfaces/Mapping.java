9767cf6e2be3d2ebea0f47af0fa92756505f8ba0
Robin Glauser <robin.glauser@gmail.com>
2018-04-03 23:34:28 +0200

    Adding documentation.

--- src/main/java/project9/interfaces/Mapping.java
+++ src/main/java/project9/interfaces/Mapping.java
@@ -3,6 +3,11 @@ package project9.interfaces;
 @FunctionalInterface
 public interface Mapping<E, F> {
 
+	/**
+	 * Method to convert element E to element F
+	 * @param element E
+	 * @return Element F
+	 */
 	public F apply(E element);
 
 }
--- src/main/java/project9/interfaces/Predicate.java
+++ src/main/java/project9/interfaces/Predicate.java
@@ -3,6 +3,11 @@ package project9.interfaces;
 @FunctionalInterface
 public interface Predicate<E> {
 
+	/**
+	 * Test whether element E meets certain conditions.
+	 * @param element Element E
+	 * @return Whether element E meets the condition.
+	 */
 	public boolean test(E element);
 
 }
