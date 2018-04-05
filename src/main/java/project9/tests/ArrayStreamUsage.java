b949b7d9f565c09c22fbf0078a5ea8f5a3a1f527
Robin Glauser <robin.glauser@gmail.com>
2018-04-05 09:28:52 +0200

    Adding more tests.

--- src/main/java/project9/tests/ArrayStreamUsage.java
+++ src/main/java/project9/tests/ArrayStreamUsage.java
@@ -3,16 +3,37 @@ package project9.tests;
 import project9.classes.ArrayStream;
 import project9.interfaces.Stream;
 
+import java.awt.*;
 import java.util.Arrays;
 
 public class ArrayStreamUsage {
 
     public static void main(String[] args) {
-        ArrayStream<Integer> arrayStream = new ArrayStream<>(1, 4, 1, 3, 4, 1, 4, 12, 1, 2);
-        System.out.println(Arrays.toString(arrayStream.filter(x -> x > 10).toList().toArray()));
-        System.out.println(Arrays.toString(arrayStream.map(x -> x > 10).toList().toArray()));
+        ArrayStream<Integer> arrayStream = new ArrayStream<>(1,2,3,4,5,6,7,8,9,10);
+
+        System.out.println(Arrays.toString(arrayStream.filter(x -> x > 5).toList().toArray()));
+
+        System.out.println(Arrays.toString(arrayStream.map(x -> x > 5).toList().toArray()));
+
         System.out.println(arrayStream.reduce((x, y) -> x + y));
+
         System.out.println(arrayStream.filter(x -> x > 10).countAll());
+
+        ArrayStream<Rectangle> rectangleArrayStream = new ArrayStream<>(
+                new Rectangle(13, 55, 10, 10),
+                new Rectangle(55, 24, 10, 10),
+                new Rectangle(10, 56, 10, 10),
+                new Rectangle(23, 10, 10, 10),
+                new Rectangle(15, 10, 10, 10)
+        );
+
+        System.out.println(
+                rectangleArrayStream
+                        .filter(x -> x.x > 12 && x.y > 12)
+                        .map(x -> "[x:" + x.x + " y:" + x.y + "]")
+                        .reduce((x, y) -> x + " " + y)
+        );
+
 
         ArrayStream<String> stringArrayStream = new ArrayStream<>("Bob", "Alice", "Eve", "Fred", "Markus");
         System.out.println(Arrays.toString(stringArrayStream.toList().toArray()));
@@ -21,7 +42,7 @@ public class ArrayStreamUsage {
         System.out.println(Arrays.toString(stringArrayStream.map(String::length).toList().toArray()));
 
         Stream<String> a = stringArrayStream.filter(x -> x.matches(".*e.*")).map(String::toUpperCase);
-        String joined = stringArrayStream.filter(x -> x.matches(".*e.*")).map(String::toUpperCase).reduce((x, y) -> y+x);
+        String joined = stringArrayStream.filter(x -> x.matches(".*e.*")).map(String::toLowerCase).reduce((x, y) -> x + ", " + y);
         System.out.println(Arrays.toString(a.toList().toArray()));
         System.out.println(joined);
     }
--- src/main/java/project9/tests/SeededStreamUsage.java
+++ src/main/java/project9/tests/SeededStreamUsage.java
@@ -2,6 +2,7 @@ package project9.tests;
 
 import project9.classes.SeededStream;
 
+import java.awt.*;
 import java.util.Arrays;
 
 public class SeededStreamUsage {
@@ -11,14 +12,20 @@ public class SeededStreamUsage {
         System.out.println(seededStream.countAll());
         System.out.println(Arrays.toString(seededStream.toList().toArray()));
 
-        seededStream = new SeededStream<>(1, x -> x+2, x -> x <= 10);
+        seededStream = new SeededStream<>(1, x -> x + 2, x -> x <= 10);
         System.out.println(seededStream.countAll());
         System.out.println(Arrays.toString(seededStream.toList().toArray()));
 
 
-        seededStream = new SeededStream<>(2, x -> x*x, x -> x <= 50);
+        seededStream = new SeededStream<>(2, x -> x * x, x -> x <= 50);
         System.out.println(seededStream.countAll());
         System.out.println(Arrays.toString(seededStream.toList().toArray()));
+
+
+        SeededStream<Rectangle> rectangleSeededStream = new SeededStream<>(new Rectangle(1, 1), x -> new Rectangle(x.width *2 , x.height * 2), x -> x.height < 100);
+
+        System.out.println(rectangleSeededStream.map(x -> "[height: " + x.height + " width: " + x.width + "]\n")
+                .reduce((x, y) -> y+x));
 
     }
 }
